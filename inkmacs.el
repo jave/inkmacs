387e3b661b61b479e451bab9c0292a49697970aa
jave <joakim@verona.se>
2012-03-08 00:22:54 +0100

    docs and bug fixes

--- inkmacs.el
+++ inkmacs.el
@@ -17,6 +17,7 @@
 (require 'org)
 (require 'org-exp)
 
+;;TODO this doesnt work out of the box for uninstalled inkscape
 (defcustom inkscape-path
   "inkscape"
   "Path to dbus-enabled inkscape.")
@@ -98,12 +99,12 @@ PREFIX can be inkapp- or inkdoc- for
                "/org/inkscape/application" t)))
     obj))
 
-(defun inkscape-document-dbus-proxy-create (desktop)
+(defun inkscape-document-dbus-proxy-create (desktop &optional session)
   "Create dbus-proxy to talk to inkscape DESKTOP.
 slow the first time, then not so bad."
   (let* ((dbus-proxy-transform-method-name-function (lambda (name) (inkscape-transform-method-name "inkdoc" name)))
          (obj (dbus-proxy-make-remote-proxy
-               :session "org.inkscape"
+               :session (or session "org.inkscape")
                (concat "/org/inkscape/" desktop) t)))
     obj))
 
@@ -125,7 +126,7 @@ slow the first time, then not so bad."
     (setq inkscape-desktop-instances (acons file-name inkscape-desktop-instance inkscape-desktop-instances))
     ;;todo inkdoc-load doesnt like if theres no actual file
     (unless (file-exists-p file-name)
-        (inkmacs-create-empty-svg file-name))
+      (inkmacs-create-empty-svg file-name))
     (inkdoc-load inkscape-desktop-instance file-name)))
 
 (defun inkmacs-create-empty-svg (file-name)
@@ -155,8 +156,8 @@ slow the first time, then not so bad."
   "Check if the desktop is alive."
   ;;todo make this method actually work
   (dbus-introspect-get-method-names  :session "org.inkscape"
-                                       (oref this object)
-                                       "org.inkscape.document"))
+                                     (oref this object)
+                                     "org.inkscape.document"))
 
 (defun inkscape-desktop ()
   "Return the inkscape desktop suitable for the context.
@@ -201,6 +202,8 @@ null if there is no desk. error if there is a broken desk."
 ;; old nodes will retain their placement
 (defvar inkorg-x 0)
 (defvar inkorg-y 0)
+
+
 
 (defun inkorg-create-or-update-text (do-tree)
   "Update the current org node or subtree.
@@ -247,8 +250,8 @@ Argument INKORG-SELECT filters the nodes to select."
   (save-excursion
     (org-back-to-heading)
     (setq inkorg-select-start-level (org-outline-level))
-    (unless (or (= 1 (org-outline-level)) (equal inkorg-select 'keep-subtree))
-      (org-up-heading-all 100));;TODO refactor. support EXPORT_FILE_NAME
+    (unless (equal inkorg-select 'keep-subtree)
+      (inkorg-root-node));;TODO refactor. support EXPORT_FILE_NAME
     (org-map-entries (lambda () (inkorg-select-node text-or-flow))
                      nil 'tree 'inkorg-select-skip)))
 
@@ -308,24 +311,38 @@ asterisks and properties are removed."
   (setq inkorg-y (+ 200 inkorg-y))
 
   ;;create text node
-  (let* ((text (inkorg-entry-text);;TODO enable different text extraction functions
+  ;;there are some issues with layers. id like to create objs 1st and set layers later
+  ;;but thats not reliable
+  ;;layer movement is overall being annoying
+  (let* ((desk (inkscape-desktop))
+         (text (inkorg-entry-text);;TODO enable different text extraction functions
                )
-         (id (inkorg-text-id (org-id-get nil t)))
-         (flow-node (inkdoc-rectangle (inkscape-desktop) inkorg-x inkorg-y 200 200))  ;; create text flow rectangle TODO enable size formatting
+         (dummy     (inkdoc-layer-set desk "inkmacs-flow-layer")    )
+         (flow-node (inkdoc-rectangle desk inkorg-x inkorg-y 200 200))  ;; create text flow rectangle TODO enable size formatting
          (flow-id (inkorg-flow-id (org-id-get nil t)))
-         (text-node (inkdoc-text (inkscape-desktop) inkorg-x inkorg-y text)))
-    (inkdoc-set-attribute (inkscape-desktop) text-node "id" id)
-    (inkdoc-set-attribute (inkscape-desktop) flow-node "id" flow-id)
+         (dummy     (inkdoc-layer-set desk "inkmacs-text-layer")    );;TODO this is redundant because flow joins stuff... se later
+         (text-node (inkdoc-text desk inkorg-x inkorg-y text))
+         (text-id (inkorg-text-id (org-id-get nil t))))
+    ;;(inkdoc-move-to-layer desk text-node "inkmacs-text-layer")
+    
+    (inkdoc-set-attribute desk text-node "id" text-id)
+    ;;(inkdoc-move-to-layer desk flow-node "inkmacs-flow-layer")
+    (inkdoc-set-attribute desk flow-node "id" flow-id)
     ;;link text flow frame and text node
-    (inkdoc-set-color (inkscape-desktop) flow-id 255 255 255 t) ;;TODO enable formatting of flow frame
+    (inkdoc-set-color desk flow-id 255 255 255 t) ;;TODO enable formatting of flow frame
+    (inkdoc-set-attribute desk flow-id "style" "color:#000000;fill:none;stroke:#7c7c7c;stroke-width:1;stroke-miterlimit:4;stroke-opacity:1;stroke-dasharray:none;stroke-dashoffset:0;marker:none;visibility:visible;display:inline;overflow:visible;enable-background:accumulate;fill-opacity:1")
     ;;   select both objects
-    (inkdoc-selection-set-list (inkscape-desktop) (list flow-id id))
-    (inkverb-object-flow-text (inkscape-desktop)) ;;text sshall be flowed in the frame
+    (inkdoc-selection-set-list desk (list flow-id text-id))
+    (inkverb-object-flow-text desk) ;;text sshall be flowed in the frame
     ;; were not finished because the text id has changed so change it back
     ;; we rely on the new flow object being selected which seems fragile
-    (inkdoc-set-attribute (inkscape-desktop)     (car (inkdoc-selection-get (inkscape-desktop))) "id" id)
-    (inkdoc-selection-clear  (inkscape-desktop))
+    (inkdoc-set-attribute desk     (car (inkdoc-selection-get desk)) "id" text-id)
+    (inkdoc-selection-clear  desk)
+    ;;moving text to text layer needs to be last since the flow operation joins flow and text
+    ;; so this splits into 2 layers
+    (inkdoc-move-to-layer desk text-id "inkmacs-text-layer");;TODO this inkscape patch didnt get merged upstream yet. check progress
     ))
+
 
 (defun inkorg-create-or-update-text-node ()
   "create a corresponding inkscape text node from the current org
@@ -336,14 +353,14 @@ node, or update the node if it already exists."
     (if (inkmacs-node-exists (inkscape-desktop) id)
         (inkdoc-set-text (inkscape-desktop) id (inkorg-entry-text))
       (inkorg-create-text-node))
-    (inkorg-apply-text-formatting id 0 (string-match "\n" text));; TODO allow different styles. this one just bollds up to 1st newline
-))
+    (inkorg-apply-text-formatting id 0 (string-match "\n" text))
+    ;; TODO allow different styles. this one just bollds up to 1st newline
+    ))
 
 (defun inkorg-apply-text-formatting (text-id range-start range-end )
   "experimental. needs patched inkscape"
   (inkdoc-text-apply-style (inkscape-desktop) text-id
-                           range-start range-end "font-weight" "bold")    
-  )
+                           range-start range-end "font-weight" "bold"))
 
 
 (defun inkorg-export-svg-1.1 ()
@@ -360,15 +377,20 @@ Then it should render well in batik and firefox hopefully."
     (select all flow nodes)
     (inkorg-flow-to-text)
     (save the file)
-    (scour the file))
-  )
+    (scour the file)))
+
 (defun inkorg-flow-to-text ()
   "Convert selected nodes from flow to text. This should normaly
   preserve formatting. This is useful for converting to a SVG 1.1
   format."
   (interactive)
-  (inkverb-object-flowtext-to-text (inkscape-desktop))
-  )
+  (inkverb-object-flowtext-to-text (inkscape-desktop)))
+
+(defun inkorg-flow-text-height (flowtext-id)
+  "figure out the current height of a flowtext node"
+  (interactive)
+  (inkverb-object-flowtext-to-text (inkscape-desktop)))
+
 
 (defun inkorg-svg-file-name ()
   "Figure out which svg file to use in this context."
@@ -376,24 +398,26 @@ Then it should render well in batik and firefox hopefully."
   ;;				 (org-entry-get (region-beginning)  "EXPORT_FILE_NAME" t)
   (save-excursion
     (let
-        ((export-file-name (org-entry-get  (region-beginning)  "EXPORT_FILE_NAME" t)))
-      ;;org-entry-property-inherited-from TODO
-      (move-beginning-of-line nil)
-      (unless  (= 1 (org-outline-level)) 
-        (org-up-heading-all 100));;TODO use inkorg-root-node
+        ((fileroot (mapconcat (lambda (e) e) (butlast (split-string (buffer-file-name ) "/")) "/"))
+         (export-file-name (org-entry-get  (point)  "EXPORT_FILE_NAME" t)))
+      (inkorg-root-node)
       (let ((file-name (concat  (or export-file-name
-                                     (org-get-heading))
+                                    (org-get-heading))
                                 ".svg")))
         (set-text-properties 0 (length file-name) nil file-name)
-        (concat (mapconcat (lambda (e) e) (butlast (split-string (buffer-file-name ) "/")) "/") "/" file-name)))))
+        ;;TODO support relative and absolute filenames
+        (if (equal "/" (substring file-name 0 1))
+            (setq fileroot ""))
+        (concat fileroot "/" file-name)))))
 
 
 (defun inkorg-root-node ()
   "Move point to the image root.
 This is either a node with EXPORT_FILE_NAME set or the level 1 parent."
+  ;;TODO thus does not reliably find the property
   (interactive)
   (let
-      ((export-file-name (org-entry-get  (region-beginning)  "EXPORT_FILE_NAME" t))
+      ((export-file-name (org-entry-get  (point)  "EXPORT_FILE_NAME" t))
        (ex-point org-entry-property-inherited-from))
     (if export-file-name
         (goto-char  ex-point)
@@ -403,6 +427,9 @@ This is either a node with EXPORT_FILE_NAME set or the level 1 parent."
         (unless  (= 1 (org-outline-level)) 
           (org-up-heading-all 100))))))
 
+
+
+
 (defun inkorg-delete-orphans ()
   ;;TODO
   ;; - figure out all inkscape nodes somewhen created by inkmacs, set A
@@ -411,10 +438,9 @@ This is either a node with EXPORT_FILE_NAME set or the level 1 parent."
   ;; this is potentially destructive so prompt for each inksscape node to be removed
   )
 
-  
+
 (define-minor-mode inkorg-mode "inkorg" nil " inkorg"
-  '(( "\e\C-x" . inkorg-create-or-update-text))    
-  )
+  '(( "\e\C-x" . inkorg-create-or-update-text)))
 
 (defun inkmacs-node-exists (desk name)
   "See if an inkscape object exists.
@@ -437,16 +463,13 @@ Argument NAME name of object."
     (inkdoc-set-attribute (inkscape-desktop) path "inkscape:connection-end-point" "d4")
     (inkdoc-set-attribute (inkscape-desktop) path "inkscape:connector-type" "polyline")
     (inkdoc-set-attribute (inkscape-desktop) path "style"
-                          "fill:none;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
-                          )
-    )
-  ) 
-  
+                          "fill:none;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"))) 
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; presentation
 (defun inkmacs-zoom-id (id)
   "zoom the object with the provided id."
-  (interactive "sid:")
+  (interactive (list (completing-read "view id:" (inkmacs-get-view-ids))))
   (inkdoc-selection-set (inkscape-desktop) id)
   (inkverb-zoom-selection  (inkscape-desktop)))
 
@@ -456,13 +479,15 @@ Argument NAME name of object."
                                   r1x0 r1y0 r1x1 r1y1)
   (inkdoc-document-set-display-area (inkscape-desktop) r0x0 r0y0 r0x1 r0y1 1.0)
   (let* ((step 10)
+         (delay 0.5) ;;you probably need to tweak step and delay
          (s1 (/ (- r1x0 r0x0 ) step))
          (s2 (/ (- r1y0 r0y0 ) step))
          (s3 (/ (- r1x1 r0x1 ) step))
          (s4 (/ (- r1y1 r0y1 ) step)))
     
     (loop for i from 0 to step do
-         (inkdoc-document-set-display-area (inkscape-desktop) (+ r0x0 (* i s1)) (+ r0y0 (* i s2)) (+ r0x1 (* i s3)) (+ r0y1 (* i s4)) 1.0))))
+          (inkdoc-document-set-display-area (inkscape-desktop) (+ r0x0 (* i s1)) (+ r0y0 (* i s2)) (+ r0x1 (* i s3)) (+ r0y1 (* i s4)) 1.0)
+          (sit-for delay))))
 
 
 
@@ -472,7 +497,7 @@ Argument NAME name of object."
           (append (inkdoc-document-get-display-area (inkscape-desktop)) (list r1x0 r1y0 r1x1 r1y1))))
 
 (defun inkmacs-zoom-animate-to-view (view)
-  (interactive "sid:")
+  (interactive (list (completing-read "view id:" (inkmacs-get-view-ids))))
   (setq inkmacs-previous-view view)
   (let* ((d (inkscape-desktop))
          (x       (string-to-number (inkdoc-get-attribute d view "x")))
@@ -486,14 +511,28 @@ Argument NAME name of object."
   "user defined areas of interest"
   (org-entry-get-multivalued-property nil "views"))
 
-(defvar inkmacs-previous-view)
+(defvar inkmacs-previous-view nil)
 
 (defun inkmacs-zoom-next-view ()
+  "zoom to next view"
+  (interactive)
+  (unless inkmacs-previous-view (setq inkmacs-previous-view (car  (inkmacs-get-view-ids))))
   (let* ((last-pos (position-if (lambda (x) (equal x inkmacs-previous-view)) (inkmacs-get-view-ids)))
          (last-pos (if last-pos last-pos 0));;fugly. what it lamely tries to achieve is set last pos to 0 as a default
-         )
-    )
-  )
+         (next-view (nth (+ 1 last-pos )
+                                        (inkmacs-get-view-ids))))
+    (message "zoom to %s" next-view)
+    ( inkmacs-zoom-animate-to-view next-view)))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; template instantiation
+
+
+
+
+
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;signals. you need a very experimental inkscape patch for this
@@ -502,12 +541,86 @@ Argument NAME name of object."
    :session "org.inkscape" "/org/inkscape/desktop_0"
    "org.inkscape.document" "object_moved"
    'inkmacs-signal-handler
-))
+   ))
 
 (defun inkmacs-signal-handler (id)
   (let ((x       (inkdoc-get-attribute inkscape-desktop-dummy id "x"))
         (y       (inkdoc-get-attribute inkscape-desktop-dummy id "y")))
     (message "id:%s x:%s y:%s" id x y)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;xwidgets. you need a very very experimental inkscape patch for this
+;;as well as a very experimental emacs.
+(defvar inkmacs-xwidget-mode-map
+  (let ((map (make-sparse-keymap)))
+    ;;(define-key map [xwidget-event] 'inkmacs-xwidget-handler) ;;use the callback scheme instead
+    map)
+  
+  "Keymap for `inkmacs-xwidget-mode'.")
+
+
+
+(define-derived-mode inkmacs-xwidget-mode
+  special-mode "inkmacs-xwidget" "inkmacs xwidget mode"
+  (setq buffer-read-only t))
+
+(defun inkmacs-xwidget ()
+  (interactive)
+  (require 'xwidget)
+  (switch-to-buffer "xw-inkmacs")
+  (insert "I\n")
+  (let ((xw  (xwidget-insert (point-min)  'socket "socket" 1000  1000)))
+    (xwidget-put xw 'callback 'inkmacs-xwidget-callback))
+  (inkmacs-xwidget-mode))
+
+(defun inkmacs-xwidget-callback (xwidget xwidget-event-type)
+  (interactive)
+  (message "stuff happened to inkmacs xwidget %S" xwidget)
+  (cond ( (eq xwidget-event-type 'xembed-ready)
+          (let*
+              ((xembed-id (nth 3 last-input-event)))
+            (message "xembed ready  event: %S xw-id:%s" xembed-id xwidget)
+            (cond
+             (t ;;here we are supposed to identify the correct xwidget originated the event
+              (start-process "xembed" "*xwidget-inkscape process*" inkscape-path  (format "--socket-id=%d" xembed-id) )
+              (message "now link manually")
+              ))))))
+
+(defun inkmacs-xwidget-set-session (session)
+  (interactive "ssession:")
+  ;;TODO the linking is sketchy still
+  (set (make-local-variable 'inkscape-desktop-instance)
+       (inkscape-document-dbus-proxy-create "desktop_0" session)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun inkmacs-make-layer (desk name)
+  "make layers"
+  (let* ((layer (inkdoc-layer-new (inkscape-desktop) )))
+    (inkdoc-set-attribute desk layer "id"  name)
+    ;;now the obj has changed name so the old name wont work
+    (inkdoc-set-attribute desk name "inkscape:label"  name)))
+
+(defun inkmacs-layer-name ( parent-layer &optional sub-layer)
+  (format "inkmacs-%s-layer" (concat parent-layer (if sub-layer (concat "-" sub-layer)))))
+
+(defun inkmacs-prepare-page-layer (desk parent-layer)
+  "make text and flow layers and a new parent-layer(page layer)"
+  (interactive (list (inkscape-desktop) (read-string "page:")))
+  (inkmacs-make-layer desk parent-layer)
+  (inkmacs-make-layer desk (inkmacs-layer-name parent-layer "flow"))
+  (inkmacs-make-layer desk (inkmacs-layer-name parent-layer "text")))
+
+(defun inkmacs-add-background ()
+  (interactive)
+  ;;;TODO add bacxground layer with a rectangle. just a stub now
+  (let* ((d (inkscape-desktop))
+         (layer-name (inkdoc-layer-new d)))
+    (inkdoc-layer-change-level d "to_bottom")
+    (inkdoc-rectangle d 0 0 1000 1000)
+    )
+  )
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;test code
